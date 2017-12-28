92d290027b4b3ab1e017575a29c7cd6ce1de99a7
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-12-28 04:48:00 -0800

    Add enhanced data table customizable component

---
+++ src/components/data-table/enhanced-data-table.js
@@ -0,0 +1,357 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import keycode from 'keycode';
+import Table, {
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel
+} from 'material-ui/Table';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
+import IconButton from 'material-ui/IconButton';
+import Tooltip from 'material-ui/Tooltip';
+import DeleteIcon from 'material-ui-icons/Delete';
+import FilterListIcon from 'material-ui-icons/FilterList';
+import TextField from 'material-ui/TextField';
+
+let counter = 0;
+function createData(name, calories, fat, carbs, protein) {
+  counter += 1;
+  return { id: counter, name, calories, fat, carbs, protein };
+}
+
+const columnData = [
+  {
+    id: 'name',
+    numeric: false,
+    disablePadding: true,
+    label: 'Dessert (100g serving)'
+  },
+  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
+  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
+  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
+  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' }
+];
+
+class EnhancedDataTableHead extends Component {
+  static propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.string.isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired
+  };
+
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render() {
+    const { order, orderBy, numSelected, rowCount, inputData } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          {columnData.map(column => {
+            return (
+              <TableCell
+                key={column.id}
+                numeric={column.numeric}
+                disablePadding={column.disablePadding}
+                sortDirection={orderBy === column.id ? order : false}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={column.numeric ? 'bottom-end' : 'bottom-start'}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === column.id}
+                    direction={order}
+                    onClick={this.createSortHandler(column.id)}
+                  >
+                    {column.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            );
+          }, this)}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+const toolbarStyles = theme => ({
+  root: {
+    paddingRight: 2
+  },
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+          color: theme.palette.secondary.A700,
+          backgroundColor: theme.palette.secondary.A100
+        }
+      : {
+          color: theme.palette.secondary.A100,
+          backgroundColor: theme.palette.secondary.A700
+        },
+  spacer: {
+    flex: '1 1 100%'
+  },
+  actions: {
+    color: theme.palette.text.secondary
+  },
+  title: {
+    flex: '0 0 auto'
+  }
+});
+
+let EnhancedDataTableToolbar = props => {
+  const { numSelected, classes, value, handleSearch } = props;
+
+  return (
+    <Toolbar
+      className={classNames(classes.root, {
+        [classes.highlight]: numSelected > 0
+      })}
+    >
+      <div>
+        <TextField placeholder="Search" onChange={handleSearch} value={value} />
+      </div>
+    </Toolbar>
+  );
+};
+
+EnhancedDataTableToolbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired
+};
+
+EnhancedDataTableToolbar = withStyles(toolbarStyles)(EnhancedDataTableToolbar);
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3
+  },
+  table: {
+    minWidth: 800
+  },
+  tableWrapper: {
+    overflowX: 'auto'
+  }
+});
+
+class EnhancedDataTable extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      order: 'asc',
+      orderBy: 'calories',
+      selected: [],
+      searchValue: '',
+      data: [
+        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+        createData('Eclair', 262, 16.0, 24, 6.0),
+        createData('Cupcake', 305, 3.7, 67, 4.3),
+        createData('Gingerbread', 356, 16.0, 49, 3.9),
+        createData('Donut', 452, 25.0, 51, 4.9),
+        createData('Honeycomb', 408, 3.2, 87, 6.5),
+        createData('Jelly Bean', 375, 0.0, 94, 0.0),
+        createData('KitKat', 518, 26.0, 65, 7.0),
+        createData('Lollipop', 392, 0.2, 98, 0.0),
+        createData('Marshmallow', 318, 0, 81, 2.0),
+        createData('Nougat', 360, 19.0, 9, 37.0),
+        createData('Oreo', 437, 18.0, 63, 4.0)
+      ],
+      filterData: [
+        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+        createData('Eclair', 262, 16.0, 24, 6.0),
+        createData('Cupcake', 305, 3.7, 67, 4.3),
+        createData('Gingerbread', 356, 16.0, 49, 3.9),
+        createData('Donut', 452, 25.0, 51, 4.9),
+        createData('Honeycomb', 408, 3.2, 87, 6.5),
+        createData('Jelly Bean', 375, 0.0, 94, 0.0),
+        createData('KitKat', 518, 26.0, 65, 7.0),
+        createData('Lollipop', 392, 0.2, 98, 0.0),
+        createData('Marshmallow', 318, 0, 81, 2.0),
+        createData('Nougat', 360, 19.0, 9, 37.0),
+        createData('Oreo', 437, 18.0, 63, 4.0)
+      ],
+      page: 0,
+      rowsPerPage: 5
+    };
+  }
+
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    const filterData =
+      order === 'desc'
+        ? this.state.filterData.sort(
+            (a, b) => (b[orderBy] < a[orderBy] ? -1 : 1)
+          )
+        : this.state.filterData.sort(
+            (a, b) => (a[orderBy] < b[orderBy] ? -1 : 1)
+          );
+
+    this.setState({ filterData, order, orderBy });
+  };
+
+  handleKeyDown = (event, id) => {
+    if (keycode(event) === 'space') {
+      this.handleClick(event, id);
+    }
+  };
+
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    this.setState({ selected: newSelected });
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  isSelected = id => this.state.selected.indexOf(id) !== -1;
+
+  handleSearch = event => {
+    const { data } = this.state;
+    let filteredDatas = [];
+    filteredDatas = data.filter(e => {
+      let mathesItems = Object.values(e);
+      let retVal = true;
+      mathesItems.forEach(e => {
+        const regex = new RegExp(event.target.value, 'gi');
+        if (typeof e == 'string') retVal = e.match(regex);
+      });
+      return retVal;
+    });
+    this.setState({
+      filterData: filteredDatas,
+      searchValue: event.target.value
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const {
+      filterData,
+      order,
+      orderBy,
+      selected,
+      rowsPerPage,
+      page
+    } = this.state;
+    const emptyRows =
+      rowsPerPage -
+      Math.min(rowsPerPage, filterData.length - page * rowsPerPage);
+
+    return (
+      <Paper className={classes.root}>
+        <EnhancedDataTableToolbar
+          numSelected={selected.length}
+          handleSearch={this.handleSearch}
+          value={this.searchValue}
+        />
+        {/*
+         */}
+        <div className={classes.tableWrapper}>
+          <Table>
+            <EnhancedDataTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={this.handleRequestSort}
+              rowCount={filterData.length}
+            />
+            <TableBody>
+              {filterData
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map(n => {
+                  const isSelected = this.isSelected(n.id);
+                  return (
+                    <TableRow
+                      hover
+                      onClick={event => this.handleClick(event, n.id)}
+                      onKeyDown={event => this.handleKeyDown(event, n.id)}
+                      role=""
+                      aria-checked={isSelected}
+                      tabIndex={-1}
+                      key={n.id}
+                      selected={isSelected}
+                    >
+                      <TableCell>{n.name}</TableCell>
+                      <TableCell numeric>{n.calories}</TableCell>
+                      <TableCell numeric>{n.fat}</TableCell>
+                      <TableCell numeric>{n.carbs}</TableCell>
+                      <TableCell numeric>{n.protein}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 49 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  count={filterData.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onChangePage={this.handleChangePage}
+                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </div>
+      </Paper>
+    );
+  }
+}
+
+EnhancedDataTable.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(EnhancedDataTable);
