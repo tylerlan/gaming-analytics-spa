f6c7cda4559653c168d9632ceb6486ed535b1647
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2018-01-03 03:09:14 -0800

    Move home lifecycle method into home container

--- src/components/home/home-container.js
+++ src/components/home/home-container.js
@@ -1,3 +1,4 @@
+import { compose, lifecycle } from 'recompose';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Home from './home';
@@ -7,6 +8,7 @@ import {
   getAggregatePerDay,
   getManufactureBreakdown
 } from '../../redux/actions/thunks';
+import * as moment from 'moment';
 
 const mapStateToProps = state => {
   return {
@@ -25,4 +27,31 @@ export const mapDispatchToProps = dispatch =>
     dispatch
   );
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+const connectToStore = connect(mapStateToProps, mapDispatchToProps);
+
+// NOTE: What to do if today's date does not exist in the return form the Api? Should the UI approximate?
+const onDidMount = lifecycle({
+  async componentDidMount() {
+    // let today = moment().format('YYYY/MM/DD');
+    let today = '2017/12/17'; // the API does not currently support later dates than this
+    let threeMonthsAgo = moment()
+      .subtract(30, 'days')
+      .format('YYYY/MM/DD');
+
+    const {
+      onSelectDateRange,
+      getPerUnitPerDay,
+      getAggregatePerDay,
+      getManufactureBreakdown
+    } = this.props;
+    // Update selected range
+    await onSelectDateRange([threeMonthsAgo, today]);
+
+    // Get data from the last 3 months
+    await getPerUnitPerDay([threeMonthsAgo, today]);
+    await getAggregatePerDay([threeMonthsAgo, today]);
+    await getManufactureBreakdown([threeMonthsAgo, today]);
+  }
+});
+
+export default compose(connectToStore, onDidMount)(Home);
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
-import * as moment from 'moment';
 
 import AppHeader from '../app-header/app-header-container';
 import NavBar from '../nav-bar/nav-bar-container';
@@ -10,13 +9,6 @@ import ComponentSwitch from '../component-switch/component-switch-container';
 import MetricsOfTheDay from '../daily-stats/daily-stats-container';
 import GraphDrilldown from '../graph-drilldown/graph-drilldown-container';
 import DataTableDrilldown from '../data-table-drilldown/data-table-drilldown-container';
-
-/* FOR TABLE */
-// import SearchBar from '../search-bar/search-bar-container';
-// import DatePicker from '../date-picker/date-picker-container';
-
-/* FOR GRAPH */
-// import DateRangeSelector from '../date-range-selector/date-range-selector-container';
 
 const componentMap = {
   M: MetricsOfTheDay,
@@ -64,10 +56,9 @@ const styles = theme => ({
   content: {
     backgroundColor: theme.palette.background.default,
     width: '100%',
-    height: '100%',
     overfloow: 'scroll',
     padding: theme.spacing.unit * 3,
-    height: 'calc(100% - 56px)',
+    height: '100%',
     marginTop: 56,
     [theme.breakpoints.up('sm')]: {
       height: 'calc(100% - 64px)',
@@ -76,47 +67,17 @@ const styles = theme => ({
   }
 });
 
-class Home extends Component {
-  async componentDidMount() {
-    // let today = moment().format('YYYY/MM/DD');
-    let today = '2017/12/17'; // the API does not currently support later dates than this
-    let threeMonthsAgo = moment()
-      .subtract(3, 'months')
-      .format('YYYY/MM/DD');
-
-    const {
-      onSelectDateRange,
-      getPerUnitPerDay,
-      getAggregatePerDay,
-      getManufactureBreakdown
-    } = this.props;
-
-    // Get data from the last 3 months
-    await getPerUnitPerDay([threeMonthsAgo, today]);
-    await getAggregatePerDay([threeMonthsAgo, today]);
-    await getManufactureBreakdown([threeMonthsAgo, today]);
-
-    // Update selected range
-    await onSelectDateRange([threeMonthsAgo, today]);
-  }
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <div className={classes.root}>
-        <div className={classes.appFrame}>
-          <AppHeader classes={classes} />
-          <NavBar classes={classes} />
-          <main className={classes.content}>
-            <ComponentSwitch components={componentMap} />
-          </main>
-        </div>
-      </div>
-    );
-  }
-}
-
+const Home = ({ classes }) => (
+  <div className={classes.root} style={{ margin: '0px' }}>
+    <div className={classes.appFrame}>
+      <AppHeader classes={classes} />
+      <NavBar classes={classes} />
+      <main className={classes.content}>
+        <ComponentSwitch components={componentMap} />
+      </main>
+    </div>
+  </div>
+);
 Home.propTypes = {
   getPerUnitPerDay: PropTypes.func.isRequired,
   getAggregatePerDay: PropTypes.func.isRequired,
